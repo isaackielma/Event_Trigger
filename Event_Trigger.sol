c52c8e3b29e256448b16e7442d06c8841e37e1b3
Isaackielma <84313603+isaackielma@users.noreply.github.com>
2022-06-26 05:36:47 +0400

    Update Event_Trigger.sol
    Adding ownable functionality

--- Event_Trigger.sol
+++ Event_Trigger.sol
@@ -1,6 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.6.0;
 
+contract Ownable {
+    address payable _owner;
+
+    constructor() public {
+        _owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(isOwner(), "You are not the owner");
+        _;
+    }
+
+    function isOwner() public view returns(bool) {
+        return (msg.sender == _owner);
+    }
+}
+
 contract Item {
     uint public priceInWei;
     uint public pricePaid;
@@ -19,18 +36,17 @@ contract Item {
         require(priceInWei == msg.value, "Only full payments allowed");
         pricePaid += msg.value;
         (bool success, ) = address(parentContract).call.value(msg.value)(abi.encodeWithSignature("triggerPayment(uint256)",index));
-        require(success, "The transaction wasn't successful, canceling");
+        require(success, "The transaction was not successful, canceling");
     }
 
     fallback() external {}
 }
 
-contract ItemManager {
+contract ItemManager is Ownable {
 
     enum SupplyChainState{Created, Paid, Delivered}
     
     struct S_Item {
-        Item _item;
         string _identifier;
         uint _itemPrice;
         ItemManager.SupplyChainState _state;
@@ -40,15 +56,13 @@ contract ItemManager {
     mapping(uint => S_Item) public items;
     uint itemIndex;
 
-    event SupplyChainStep(uint _itemIndex, uint _step, address _itemAddress);
+    event SupplyChainStep(uint _itemIndex, uint _step);
 
-    function createItem(string memory _identifier, uint _itemPrice) public {
-        Item item = new Item(this, _itemPrice, itemIndex);
-        items[itemIndex]._item = item;
+    function createItem(string memory _identifier, uint _itemPrice) public onlyOwner {
         items[itemIndex]._identifier = _identifier;
         items[itemIndex]._itemPrice = _itemPrice;
         items[itemIndex]._state = SupplyChainState.Created;
-        emit SupplyChainStep(itemIndex, uint(items[itemIndex]._state), address(item));
+        emit SupplyChainStep(itemIndex, uint(items[itemIndex]._state));
         itemIndex++;
     }
 
@@ -57,14 +71,14 @@ contract ItemManager {
         require(items[_itemIndex]._state == SupplyChainState.Created, "Item is further in the chain");
         items[_itemIndex]._state = SupplyChainState.Paid;
 
-        emit SupplyChainStep(_itemIndex, uint(items[_itemIndex]._state), address(items[_itemIndex]._item));
+        emit SupplyChainStep(_itemIndex, uint(items[_itemIndex]._state));
 
     }
 
-    function triggerDelivery(uint _itemIndex) public {
+    function triggerDelivery(uint _itemIndex) public onlyOwner {
         require(items[_itemIndex]._state == SupplyChainState.Paid, "Item is further in the chain");
         items[_itemIndex]._state = SupplyChainState.Delivered;
 
-        emit SupplyChainStep(_itemIndex, uint(items[_itemIndex]._state), address(items[_itemIndex]._item));
+        emit SupplyChainStep(_itemIndex, uint(items[_itemIndex]._state));
     }
 }
